6c0f25c0643f95c49affbfde6a9fabd836341133
Andrew Buensalida <andrewcbuensalida@gmail.com>
2023-11-03 22:17:44 -0700

    enable unit tests

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -16,7 +16,7 @@ export const lambdaHandler = async (event, context) => {
         return {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world2',
+                message: 'hello world3',
             })
         }
     } catch (err) {
